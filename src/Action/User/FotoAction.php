2982e02e6d0162532737501748b45aad348e1056
emyca <evmyca@gmail.com>
2025-07-05 18:31:16 +0300

    Update user action

--- src/Action/User/FotoAction.php
+++ src/Action/User/FotoAction.php
@@ -2,24 +2,28 @@
 
 namespace App\Action\User;
 
+use App\Domain\Foto\Service\FotoReadService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Views\Twig;
 
 final class FotoAction 
 {
+    private $service;
     private $twig;
 
     public function __construct(
+        FotoReadService $service,
         Twig $twig, 
     ) {
+        $this->service = $service;
         $this->twig = $twig;
     }
 
     public function __invoke(ServerRequestInterface $request, 
         ResponseInterface $response): ResponseInterface 
     {
-        $data = array();
+        $data = $this->service->readAll();
         $viewData = array(
             'title' => 'Foto Kit',
             'items' => $data);
