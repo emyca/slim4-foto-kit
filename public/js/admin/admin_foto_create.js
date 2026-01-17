2c5a7f33bd378e1664cc511263f2392b3be4986d
emyca <evmyca@gmail.com>
2026-01-17 11:14:41 +0200

    Return to jQuery

--- public/js/admin/admin_foto_create.js
+++ public/js/admin/admin_foto_create.js
@@ -1,72 +1,70 @@
-document.addEventListener("DOMContentLoaded", function()  {
+$(function() {
 
-    document.getElementById("modal_add_btn_add").addEventListener("click", function(e) {
+    const modalAddBtnAdd = $('#modal_add_btn_add');
+    const modalAddSpinner = $('#modal_add_spinner');
+    const modalAddBtnClose = $('#modal_add_btn_close');
+    const modalAddResponse = $('#modal_add_response');
+    const errorColor = '#f02d1f';
+    const successColor = '#22a131';
+
+    modalAddBtnAdd.click(function(e) {
         e.preventDefault();
-
-        const modalAddBtnAdd = document.getElementById("modal_add_btn_add");
-        const modalAddSpinner = document.getElementById("modal_add_spinner");
-        const modalAddResponse = document.getElementById("modal_add_response");
-
-        modalAddBtnAdd.disabled = true;
-        modalAddSpinner.style.display = "block";    
-        modalAddResponse.innerHTML = '';
+        modalAddBtnAdd.prop("disabled", true);
+        modalAddSpinner.show();
 
         let formData = new FormData();
-        // formData.append('file', document.querySelector("input[name=file_add]").files[0]);
-        formData.append('file', document.getElementById("file_add").files[0]);
-        formData.append('name', document.querySelector("input[name=name_add]").value);
-        formData.append('description', document.querySelector("textarea[name=description_add]").value);
+        formData.append('file', $('input[name=file_add]')[0].files[0]);
+        formData.append('name', $('input[name=name_add]').val());
+        formData.append('description', $('textarea[name=description_add]').val());
 
-        const url = "./fotos";
-
-        fetch(url, {
+        fetch('./fotos', {
             method: 'POST',
             headers: {'Accept': 'application/json'},
             body: formData
         })
         .then(response => {
             if (!response.ok) {
-                modalAddSpinner.style.display = "none";
-                modalAddBtnAdd.disabled = false;
-                throw new Error(`HTTP Error: ${response.status}, ${response.statusText}!`);
+                modalAddSpinner.hide();
+                modalAddBtnAdd.prop("disabled", false);
+                throw new Error(`HTTP Error: ${response.status}, 
+                    ${response.statusText}!`);
             }
             return response.json();
         })
         .then(data => {
-            modalAddSpinner.style.display = "none";
-            modalAddBtnAdd.disabled = false;
+            modalAddSpinner.hide();
+            modalAddBtnAdd.prop("disabled", false);
             if(data.status == 401) {
-                modalAddResponse.style.color = "#f02d1f";
-                modalAddResponse.innerHTML = data.message;    
+                modalAddResponse.css('color', errorColor);
+                modalAddResponse.html(data.message);
                 setTimeout(function() {
                     window.location.replace(data.url);
                 }, 1000);
             } else {
                 if(data.success == false) {
-                    modalAddResponse.style.color = "#f02d1f";
-                    modalAddResponse.innerHTML = data.message;
+                    modalAddResponse.css('color', errorColor);
+                    modalAddResponse.html(data.message);
                 } else {
-                    modalAddResponse.style.color = "#22a131";
-                    modalAddResponse.innerHTML = data.message;
+                    modalAddResponse.css('color', successColor);
+                    modalAddResponse.html(data.message);
                 }
             }
         })
         .catch(error => {
-            modalAddSpinner.style.display = "none";
-            modalAddBtnAdd.disabled = false;
-            modalAddResponse.style.color = "#f02d1f";
+            modalAddSpinner.hide();
+            modalAddBtnAdd.prop("disabled", false);
+            modalAddResponse.css('color', errorColor);
             if (error.message === 'Failed to fetch') {
-                modalAddResponse.innerHTML = "Network Error!";
+                modalAddResponse.html("Network Error!");
             } else {
-                modalAddResponse.innerHTML = error.message;
+                modalAddResponse.html(error.message);
             }
-            throw error;
         });
     });
 
-    document.getElementById("modal_add_btn_close").addEventListener("click", function() {
-        document.getElementById("form_add").reset();
-        document.getElementById("modal_add_response").value = "";
+    modalAddBtnClose.click(function() {
+        $('#form_add')[0].reset();
+        modalAddResponse.html('');
         location.reload();
     });
 });
